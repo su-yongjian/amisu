33aa89df0ed65609969ca04ba6899c2d13f360e7
amisu <721765355@qq.com>
2019-05-30 15:04:02 +0800

    up

--- mongoose/controller/users.js
+++ mongoose/controller/users.js
@@ -6,14 +6,28 @@ let router = new Router()
 router.prefix('/user')
 
 router.get('/addUser',async (ctx)=>{
-  
-  let doc = await Users.create({
-    userName:'王五2',
+  let code = 0 // 状态码
+  let msg = '' // 返回内容
+  let doc = {
+    userName:'王五3',
     password:'123456',
     gender:1
-  })
-  console.log('新增成功');
-  
+  }
+  try {
+    let ret = await Users.create(doc)
+    code = 0
+    msg = '保存成功, ' + ret
+  } catch (error) {
+    code = -1
+    msg = '保存失败, ' + error
+    console.log(error)
+  }
+
+  ctx.body = {
+    code,
+    msg
+  }
+  return msg
 })
 
 
